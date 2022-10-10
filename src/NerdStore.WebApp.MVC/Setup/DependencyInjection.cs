f6a6189b2f5c55178741a3eeb652989a501d00ac
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2022-10-10 07:16:46 -0300

    Conectando a camada de apresentacao

---
+++ src/NerdStore.WebApp.MVC/Setup/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using NerdStore.Catalogo.Application.Services;
+using NerdStore.Catalogo.Domain;
+using NerdStore.Core.Bus;
+using NerdStoreCatalogo.Data.Repository;
+using NerdStoreCatalogo.Data;
+
+namespace NerdStore.WebApp.MVC.Setup
+{
+    public static class DependencyInjection
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            //Domain Bus(Mediator)
+            services.AddScoped<IMediatrHandler, MediatrHandler>();
+
+            //Catálogo
+            services.AddScoped<IProdutoRepository, ProdutoRepository>();
+            services.AddScoped<IProdutoAppService, ProdutoAppService>();
+            services.AddScoped<IEstoqueService, EstoqueService>();
+            services.AddScoped<CatalogoContext>();
+        }
+    }
+}
